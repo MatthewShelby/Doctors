10bd78308af9b751cb5bbf1f7bf37905e1c246e4
shahabi110 <shahabi110@gmail.com>
2021-11-11 20:52:58 +0330

    sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server..component';
+import { HeaderComponent } from './pages/common---/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent
+    AppComponent, ServerComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
