b467c9415485871d4afda63d54798461208127de
shahabi110 <shahabi110@gmail.com>
2021-11-11 20:28:09 +0330

    First commit-start git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server..component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ServerComponent
   ],
   imports: [
     BrowserModule,
