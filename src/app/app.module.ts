1e994934a5e3ed002753bafd14e300093d1be54b
shahabi110 <shahabi110@gmail.com>
2021-11-13 10:25:25 +0330

    Add Wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server..component';
 import { HeaderComponent } from './pages/common---/header/header.component';
+import { WrapperComponent } from './pages/common---/wrapper/wrapper.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent, HeaderComponent
+    AppComponent, ServerComponent, HeaderComponent, WrapperComponent, StartPageComponent
   ],
   imports: [
     BrowserModule,
